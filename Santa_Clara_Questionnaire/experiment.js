33a991e235ec4d4f17f4e1a840e745f57443e70d
HeRm4nV <herman.valencia.13@sansano.usm.cl>
2020-05-06 03:06:13 -0400

    New Intruments
    Se agregaron 3 nuevos instrumentos (Religious Coping, Santa Clara Questionnaire y Springfield Religious Scale) ademas se cambió el nombre del Magic Ideation a Magical Ideation, se agrega documentacion a todas las carpetas incluyendo la data para el maker

---
+++ Santa_Clara_Questionnaire/experiment.js
@@ -0,0 +1,114 @@
+/**
+ * CSCN lab
+/**
+This document was made with test_maker
+*/
+
+onkeydown = function block_fkeys(event){
+    var x = event.which || event.keyCode;
+    if(x == 112 || x == 116){
+        console.log("Blocked key");
+        event.preventDefault();
+        return false;
+    }else{
+        return;
+    }
+}
+
+questions = [];    //final timeline
+
+questions_experiment = [];    //temporal timeline
+
+var instruction_screen_experiment = {
+    type: 'instructions',
+    pages: ['<p><left><b><big>Cuestionario Santa Clara de Fuerza de Fe Religiosa</big></b><br />'+
+    'Por favor, responde las siguientes preguntas.' +'</p>'],
+    data:{trialid: 'Screen_WM'},
+    show_clickable_nav: true,
+    on_trial_start: function(){
+        bloquear_enter = 0;
+    }
+};
+
+var question01 = {
+  type: 'survey-multi-choice-horizontal',
+  questions: [{prompt: "<div class='justified'>1. Mi fe religiosa es extremadamente importante para mí.</div>", options: ['Totalmente en desacuerdo', 'En desacuerdo', 'De acuerdo', 'Totalmente de acuerdo'], required: true, horizontal: true}],
+  data: {trialid: 'Santa_Clara_Questionnaire_01'}
+};
+questions_experiment.push(question01);
+
+var question02 = {
+  type: 'survey-multi-choice-horizontal',
+  questions: [{prompt: "<div class='justified'>2. Rezo a diario.</div>", options: ['Totalmente en desacuerdo', 'En desacuerdo', 'De acuerdo', 'Totalmente de acuerdo'], required: true, horizontal: true}],
+  data: {trialid: 'Santa_Clara_Questionnaire_02'}
+};
+questions_experiment.push(question02);
+
+var question03 = {
+  type: 'survey-multi-choice-horizontal',
+  questions: [{prompt: "<div class='justified'>3. Miro a mi fe como una fuente de inspiración.</div>", options: ['Totalmente en desacuerdo', 'En desacuerdo', 'De acuerdo', 'Totalmente de acuerdo'], required: true, horizontal: true}],
+  data: {trialid: 'Santa_Clara_Questionnaire_03'}
+};
+questions_experiment.push(question03);
+
+var question04 = {
+  type: 'survey-multi-choice-horizontal',
+  questions: [{prompt: "<div class='justified'>4. Considero que mi fe proporciona significado y propósito en mi vida.</div>", options: ['Totalmente en desacuerdo', 'En desacuerdo', 'De acuerdo', 'Totalmente de acuerdo'], required: true, horizontal: true}],
+  data: {trialid: 'Santa_Clara_Questionnaire_04'}
+};
+questions_experiment.push(question04);
+
+var question05 = {
+  type: 'survey-multi-choice-horizontal',
+  questions: [{prompt: "<div class='justified'>5. Me considero activo en mi fe o iglesia.</div>", options: ['Totalmente en desacuerdo', 'En desacuerdo', 'De acuerdo', 'Totalmente de acuerdo'], required: true, horizontal: true}],
+  data: {trialid: 'Santa_Clara_Questionnaire_05'}
+};
+questions_experiment.push(question05);
+
+var question06 = {
+  type: 'survey-multi-choice-horizontal',
+  questions: [{prompt: "<div class='justified'>6. Mi fe es una parte importante de lo que soy como persona.</div>", options: ['Totalmente en desacuerdo', 'En desacuerdo', 'De acuerdo', 'Totalmente de acuerdo'], required: true, horizontal: true}],
+  data: {trialid: 'Santa_Clara_Questionnaire_06'}
+};
+questions_experiment.push(question06);
+
+var question07 = {
+  type: 'survey-multi-choice-horizontal',
+  questions: [{prompt: "<div class='justified'>7. Mi relación con Dios es extremadamente importante para mí.</div>", options: ['Totalmente en desacuerdo', 'En desacuerdo', 'De acuerdo', 'Totalmente de acuerdo'], required: true, horizontal: true}],
+  data: {trialid: 'Santa_Clara_Questionnaire_07'}
+};
+questions_experiment.push(question07);
+
+var question08 = {
+  type: 'survey-multi-choice-horizontal',
+  questions: [{prompt: "<div class='justified'>8. Me gusta estar cerca de otros que comparten mi fe.</div>", options: ['Totalmente en desacuerdo', 'En desacuerdo', 'De acuerdo', 'Totalmente de acuerdo'], required: true, horizontal: true}],
+  data: {trialid: 'Santa_Clara_Questionnaire_08'}
+};
+questions_experiment.push(question08);
+
+var question09 = {
+  type: 'survey-multi-choice-horizontal',
+  questions: [{prompt: "<div class='justified'>9. Miro a mi fe como una fuente de consuelo.</div>", options: ['Totalmente en desacuerdo', 'En desacuerdo', 'De acuerdo', 'Totalmente de acuerdo'], required: true, horizontal: true}],
+  data: {trialid: 'Santa_Clara_Questionnaire_09'}
+};
+questions_experiment.push(question09);
+
+var question10 = {
+  type: 'survey-multi-choice-horizontal',
+  questions: [{prompt: "<div class='justified'>10. Mi fe impacta muchas de mis decisiones.</div>", options: ['Totalmente en desacuerdo', 'En desacuerdo', 'De acuerdo', 'Totalmente de acuerdo'], required: true, horizontal: true}],
+  data: {trialid: 'Santa_Clara_Questionnaire_10'}
+};
+questions_experiment.push(question10);
+
+questions_experiment.unshift(instruction_screen_experiment);
+questions.push.apply(questions, questions_experiment)
+
+if(window.innerWidth != screen.width || window.innerHeight != screen.height){
+  questions.unshift({
+    type: 'fullscreen',
+    message: '<p>El experimento entrará en modo pantalla completa</p>',
+    button_label: 'Full screen',
+    delay_after: 0,
+    fullscreen_mode: true
+  });
+}
